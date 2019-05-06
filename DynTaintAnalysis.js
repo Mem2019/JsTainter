55a7610c28cab6526a260dc8abed8c02539f7572
Mem2019 <434791602@qq.com>
2019-05-06 17:49:30 +0100

    integrate functions into method, and make  as private field

--- DynTaintAnalysis.js
+++ DynTaintAnalysis.js
@@ -10,314 +10,312 @@ var config = new (function ()
 })();
 
 
-function AnnotatedValue(val, shadow)
-{
-	this.val = val;
-	this.shadow = shadow;
-}
-function actual(val)
-{
-	return val instanceof AnnotatedValue ? val.val : val;
-}
-function shadow(val, noTaint)
-{
-	if (val instanceof AnnotatedValue)
-	{
-		return val.shadow;
-	}
-	else if (typeof val === 'string')
-	{
-		var ret = [];//todo, optimize to logn
-		for (var i = 0; i < val.length; i++)
-		{
-			ret[i] = noTaint;
-		}
-		return ret;
-	}
-	else if (typeof val === 'number')
-	{
-		return noTaint;
-	}
-	return noTaint;//todo???
-}
 
-function assertTaint(val, taint, rule, position)
-{
-	function myAssert(b)
-	{
-		if (!b)
-		{
-			process.stdout.write("Assertion failure at" + position);
-			assert(false);
-		}
-	}
-	var s = shadow(val, rule.noTaint);
-	myAssert(typeof s === typeof taint);
-	if (Array.isArray(s))
-	{
-		myAssert(s.length === taint.length);
-		for (var i = 0; i < s.length; i++)
-		{
-			myAssert(s[i] === taint[i]);
-		}
-	}
-	else
-	{
-		myAssert(s === taint);
-	}
-}
-
-const numAddTypes = new Set(['undefined', 'boolean', 'number']);
-const isNumAddOperands = (val) => numAddTypes.has(typeof val) || val === null;
-/*undefined and null in array will not be shown
-when they are in array,
-and array is converted to string*/
-const isSolidStrInArr = (val, outArrs) => typeof val != 'undefined'
-	&& val !== null && outArrs.indexOf(val) === -1;
-
-function getTaintArrayForArray(arr, rule, outArrs)
-{//pre: Array.isArray(aval)
-	if (arr.length === 0)
-		return [];
-	outArrs.push(arr);
-	var ret = [];
-	for (var i = 0; i < arr.length - 1; i++)
-	{//iterate except for last element
-		if (isSolidStrInArr(actual(arr[i]), outArrs))
-		{
-			ret = ret.concat(getTaintArrayH(arr[i], rule, outArrs));
-		}
-		ret = ret.concat(false);//',' is not tainted
-	}
-	if (isSolidStrInArr(actual(arr[i]), outArrs))
-	{
-		ret = ret.concat(getTaintArrayH(arr[i], rule, outArrs));
-	}
-	//Utils.assert(ret.length === (''+arr).length);
-	//cannot be true due to AnnotatedValue Object
-	outArrs.pop();
-	return ret;
-}
-function getTaintArrayH(val, rule, outArrs)
-{//get the taint array when `val` is converted to string
-	var aval = actual(val);
-	switch(typeof aval)
-	{
-		case 'string':
-			return shadow(val, rule.noTaint);
-		case 'object':
-		{
-			if (Array.isArray(aval))
-				return getTaintArrayForArray(aval, rule, outArrs);
-			else //this includes the case where aval === null
-				return Utils.fillArray(rule.noTaint, ('' + aval).length)
-		}
-		case 'number':
-		case 'boolean':
-		case 'undefined':
-		{
-			return rule.toStringTaint(aval, shadow(val, rule.noTaint));
-		}
-		default:
-			throw Error("Currently does not support type \'" + typeof val + "\' for add");
-	}
-}
-
-function getTaintArray(val, rule)
-{
-	return getTaintArrayH(val, rule, []);
-}
-
-function addTaintProp(left, right, result, rule, op)
-{
-	if (isNumAddOperands(actual(left)) &&
-		isNumAddOperands(actual(right)))
-	{//numeric add
-		var taint_state = rule.arithmetic(
-			shadow(left, rule.noTaint), shadow(right, rule.noTaint));
-
-		process.stdout.write(actual(left) + ' ' + op + ' ' +
-			actual(right) + ' = ' + result + '; ');
-		process.stdout.write(shadow(left, rule.noTaint) + ' ' + op + ' ' +
-			shadow(right, rule.noTaint) + ' = ' + taint_state + '\n');
-
-		return new AnnotatedValue(result, taint_state);
-	}
-	else //if (typeof actual(left) === "string" &&
-		//typeof actual(right) === "string")
-	{//string concatenation
-		return new AnnotatedValue(actual(left) + actual(right),
-			getTaintArray(left, rule).concat(getTaintArray(right, rule)));
-	}
-}
-
-const numChar = new Set("0123456789xXabcdefABCDEF.-InfinityNaN");
-function alwaysGiveNaNStr(v, s, rule)
-{//pre: s is string
-	var tmp = "";
-	for (var i = 0; i < v.length; i++)
-	{
-		if (s[i] !== rule.noTaint)
-		{//if tainted, try to convert to number
-			tmp += '0';
-		}
-		else if (!numChar.has(v[i]))
-		{//if not in standard numChar, must be NaN
-			return true;
-		}
-		else
-		{//if it is standard, leave it
-			tmp += v[i];
-		}
-	}
-	return isNaN(Number(tmp));
-}
-
-const numArithTypes = new Set(['boolean', 'number']);
-function alwaysGiveNaN(rawVal, rule)
-{
-	var val = actual(rawVal);
-	var s = shadow(rawVal, rule.noTaint);
-	var t = typeof val;
-	var b = (t === 'string') && alwaysGiveNaNStr(val, s, rule);
-	/*if (b || rule.isTainted(s))
-		Log.log("Tainted String is producing NaN, " +
-			"assuming result to be untainted, " +
-			"change the input to number for higher accuracy");*/
-	return (t === 'object' && val !== null &&
-		alwaysGiveNaNStr(''+val, rule.toStringTaint(val, s), rule)) ||
-		t === 'undefined' || b || (t === 'number' && isNaN(val));
-}
-//string that will always produce NaN
-
-
-
-
-function arithTaintProp(left, right, result, rule, op)
-{
-	if (alwaysGiveNaN(left, rule) ||
-		alwaysGiveNaN(right, rule))
-	{
-		return result;//no taint
-	}
-	else
-	{//might still be false positive if s == 'true'
-		var taint_state = rule.arithmetic(
-			rule.compressTaint(shadow(left, rule.noTaint)),
-			rule.compressTaint(shadow(right, rule.noTaint)));
-
-		process.stdout.write(actual(left) + ' ' + op + ' ' +
-			actual(right) + ' = ' + result + '; ');
-		process.stdout.write(shadow(left, rule.noTaint) + ' ' + op + ' ' +
-			shadow(right, rule.noTaint) + ' = ' + taint_state + '\n');
-
-		return new AnnotatedValue(result, taint_state);
-	}
-}
-
-function isTaintAsNum(val, rule)
-{
-	if (alwaysGiveNaN(val, rule))
-		return false;
-	return rule.isTainted(shadow(val));
-}
-
-const isZeroInBitOper = (v, rule) =>
-	v === null || alwaysGiveNaN(v, rule) ||
-	(typeof v == "number" && (isNaN(v) || v === 0));
-
-function shiftTaintProp(left, right, result, rule, op)
-{
-	if (isZeroInBitOper(left, rule))
-	{//if LHS is always NaN, result is always 0
-		return result;
-	}
-	else
-	{//might still be false positive if s == 'true'
-		var taint_state = rule.arithmetic(
-			rule.compressTaint(shadow(left, rule.noTaint)),
-			rule.compressTaint(shadow(right, rule.noTaint)));
-
-		process.stdout.write(actual(left) + ' ' + op + ' ' +
-			actual(right) + ' = ' + result + '; ');
-		process.stdout.write(shadow(left, rule.noTaint) + ' ' + op + ' ' +
-			shadow(right, rule.noTaint) + ' = ' + taint_state + '\n');
-
-		return new AnnotatedValue(result, taint_state);
-	}
-}
-
-function cmpTaintProp(left, right, result, rule, op)
-{//todo, consider more cases to improve accuracy
-	var taint_state = rule.arithmetic(
-	rule.compressTaint(shadow(left, rule.noTaint)),
-	rule.compressTaint(shadow(right, rule.noTaint)));
-
-	process.stdout.write(actual(left) + ' ' + op + ' ' +
-		actual(right) + ' = ' + result + '; ');
-	process.stdout.write(shadow(left, rule.noTaint) + ' ' + op + ' ' +
-		shadow(right, rule.noTaint) + ' = ' + taint_state + '\n');
-
-	return new AnnotatedValue(result, taint_state);
-}
-
-
-function binaryRec(left, right, taintProp, sandbox, iid)
-{//todo
-	/*if (isTainted(shadow(left, noTaint)) || isTainted(shadow(right, noTaint)))
-	{
-		taintProp[(sandbox.iidToLocation(
-			sandbox.getGlobalIID(iid)))] = 1;
-	}*/
-}
-function funcInvokeRec(func, base, args)
-{
-
-}
-
-function taintAll(val)
-{
-	if (typeof val == 'object')
-	{//todo: should we change strategy
-		ret = Array.isArray(val) ? [] : {};
-		for (var k in val)
-		{
-			taintAll(val[k]);
-		}
-	}
-}
-function untaintAll()
-{
-	//todo
-}
-
-function andTaintProp(left, right, result, rule, op)
-{
-	if (isZeroInBitOper(left, rule) ||
-		 isZeroInBitOper(right, rule))
-	{
-		return result;
-	}
-	else
-	{
-		var taint_state = rule.arithmetic(
-		rule.compressTaint(shadow(left, rule.noTaint)),
-		rule.compressTaint(shadow(right, rule.noTaint)));
-
-		process.stdout.write(actual(left) + ' ' + op + ' ' +
-			actual(right) + ' = ' + result + '; ');
-		process.stdout.write(shadow(left, rule.noTaint) + ' ' + op + ' ' +
-			shadow(right, rule.noTaint) + ' = ' + taint_state + '\n');
-
-		return new AnnotatedValue(result, taint_state);
-	}
-}
 
 
 (function (sandbox)
 {
 function TaintAnalysis(rule)
 {
+	function AnnotatedValue(val, shadow)
+	{
+		this.val = val;
+		this.shadow = shadow;
+	}
+	function actual(val)
+	{
+		return val instanceof AnnotatedValue ? val.val : val;
+	}
+	function shadow(val, noTaint)
+	{
+		if (val instanceof AnnotatedValue)
+		{
+			return val.shadow;
+		}
+		else if (typeof val === 'string')
+		{
+			var ret = [];//todo, optimize to logn
+			for (var i = 0; i < val.length; i++)
+			{
+				ret[i] = noTaint;
+			}
+			return ret;
+		}
+		else if (typeof val === 'number')
+		{
+			return noTaint;
+		}
+		return noTaint;//todo???
+	}
+
+	function assertTaint(val, taint, position)
+	{
+		function myAssert(b)
+		{
+			if (!b)
+			{
+				process.stdout.write("Assertion failure at" + position);
+				assert(false);
+			}
+		}
+		var s = shadow(val, rule.noTaint);
+		myAssert(typeof s === typeof taint);
+		if (Array.isArray(s))
+		{
+			myAssert(s.length === taint.length);
+			for (var i = 0; i < s.length; i++)
+			{
+				myAssert(s[i] === taint[i]);
+			}
+		}
+		else
+		{
+			myAssert(s === taint);
+		}
+	}
+
+	const numAddTypes = new Set(['undefined', 'boolean', 'number']);
+	const isNumAddOperands = (val) => numAddTypes.has(typeof val) || val === null;
+	/*undefined and null in array will not be shown
+	when they are in array,
+	and array is converted to string*/
+	const isSolidStrInArr = (val, outArrs) => typeof val != 'undefined'
+		&& val !== null && outArrs.indexOf(val) === -1;
+
+	function getTaintArrayForArray(arr, outArrs)
+	{//pre: Array.isArray(aval)
+		if (arr.length === 0)
+			return [];
+		outArrs.push(arr);
+		var ret = [];
+		for (var i = 0; i < arr.length - 1; i++)
+		{//iterate except for last element
+			if (isSolidStrInArr(actual(arr[i]), outArrs))
+			{
+				ret = ret.concat(getTaintArrayH(arr[i], outArrs));
+			}
+			ret = ret.concat(false);//',' is not tainted
+		}
+		if (isSolidStrInArr(actual(arr[i]), outArrs))
+		{
+			ret = ret.concat(getTaintArrayH(arr[i], outArrs));
+		}
+		//Utils.assert(ret.length === (''+arr).length);
+		//cannot be true due to AnnotatedValue Object
+		outArrs.pop();
+		return ret;
+	}
+	function getTaintArrayH(val, outArrs)
+	{//get the taint array when `val` is converted to string
+		var aval = actual(val);
+		switch(typeof aval)
+		{
+			case 'string':
+				return shadow(val, rule.noTaint);
+			case 'object':
+			{
+				if (Array.isArray(aval))
+					return getTaintArrayForArray(aval, outArrs);
+				else //this includes the case where aval === null
+					return Utils.fillArray(rule.noTaint, ('' + aval).length)
+			}
+			case 'number':
+			case 'boolean':
+			case 'undefined':
+			{
+				return rule.toStringTaint(aval, shadow(val, rule.noTaint));
+			}
+			default:
+				throw Error("Currently does not support type \'" + typeof val + "\' for add");
+		}
+	}
+
+	function getTaintArray(val)
+	{
+		return getTaintArrayH(val, []);
+	}
+
+	function addTaintProp(left, right, result, op)
+	{
+		if (isNumAddOperands(actual(left)) &&
+			isNumAddOperands(actual(right)))
+		{//numeric add
+			var taint_state = rule.arithmetic(
+				shadow(left, rule.noTaint), shadow(right, rule.noTaint));
+
+			process.stdout.write(actual(left) + ' ' + op + ' ' +
+				actual(right) + ' = ' + result + '; ');
+			process.stdout.write(shadow(left, rule.noTaint) + ' ' + op + ' ' +
+				shadow(right, rule.noTaint) + ' = ' + taint_state + '\n');
+
+			return new AnnotatedValue(result, taint_state);
+		}
+		else //if (typeof actual(left) === "string" &&
+		//typeof actual(right) === "string")
+		{//string concatenation
+			return new AnnotatedValue(actual(left) + actual(right),
+				getTaintArray(left, rule).concat(getTaintArray(right, rule)));
+		}
+	}
+
+	const numChar = new Set("0123456789xXabcdefABCDEF.-InfinityNaN");
+	function alwaysGiveNaNStr(v, s)
+	{//pre: s is string
+		var tmp = "";
+		for (var i = 0; i < v.length; i++)
+		{
+			if (s[i] !== rule.noTaint)
+			{//if tainted, try to convert to number
+				tmp += '0';
+			}
+			else if (!numChar.has(v[i]))
+			{//if not in standard numChar, must be NaN
+				return true;
+			}
+			else
+			{//if it is standard, leave it
+				tmp += v[i];
+			}
+		}
+		return isNaN(Number(tmp));
+	}
+
+	const numArithTypes = new Set(['boolean', 'number']);
+	function alwaysGiveNaN(rawVal)
+	{
+		var val = actual(rawVal);
+		var s = shadow(rawVal, rule.noTaint);
+		var t = typeof val;
+		var b = (t === 'string') && alwaysGiveNaNStr(val, s);
+		/*if (b || rule.isTainted(s))
+			Log.log("Tainted String is producing NaN, " +
+				"assuming result to be untainted, " +
+				"change the input to number for higher accuracy");*/
+		return (t === 'object' && val !== null &&
+			alwaysGiveNaNStr(''+val, rule.toStringTaint(val, s), rule)) ||
+			t === 'undefined' || b || (t === 'number' && isNaN(val));
+	}
+//string that will always produce NaN
+
+	function arithTaintProp(left, right, result, op)
+	{
+		if (alwaysGiveNaN(left) ||
+			alwaysGiveNaN(right))
+		{
+			return result;//no taint
+		}
+		else
+		{//might still be false positive if s == 'true'
+			var taint_state = rule.arithmetic(
+				rule.compressTaint(shadow(left, rule.noTaint)),
+				rule.compressTaint(shadow(right, rule.noTaint)));
+
+			process.stdout.write(actual(left) + ' ' + op + ' ' +
+				actual(right) + ' = ' + result + '; ');
+			process.stdout.write(shadow(left, rule.noTaint) + ' ' + op + ' ' +
+				shadow(right, rule.noTaint) + ' = ' + taint_state + '\n');
+
+			return new AnnotatedValue(result, taint_state);
+		}
+	}
+
+	function isTaintAsNum(val)
+	{
+		if (alwaysGiveNaN(val))
+			return false;
+		return rule.isTainted(shadow(val));
+	}
+
+	const isZeroInBitOper = (v) =>
+		v === null || alwaysGiveNaN(v) ||
+		(typeof v == "number" && (isNaN(v) || v === 0));
+
+	function shiftTaintProp(left, right, result, op)
+	{
+		if (isZeroInBitOper(left))
+		{//if LHS is always NaN, result is always 0
+			return result;
+		}
+		else
+		{//might still be false positive if s == 'true'
+			var taint_state = rule.arithmetic(
+				rule.compressTaint(shadow(left, rule.noTaint)),
+				rule.compressTaint(shadow(right, rule.noTaint)));
+
+			process.stdout.write(actual(left) + ' ' + op + ' ' +
+				actual(right) + ' = ' + result + '; ');
+			process.stdout.write(shadow(left, rule.noTaint) + ' ' + op + ' ' +
+				shadow(right, rule.noTaint) + ' = ' + taint_state + '\n');
+
+			return new AnnotatedValue(result, taint_state);
+		}
+	}
+
+	function cmpTaintProp(left, right, result, op)
+	{//todo, consider more cases to improve accuracy
+		var taint_state = rule.arithmetic(
+			rule.compressTaint(shadow(left, rule.noTaint)),
+			rule.compressTaint(shadow(right, rule.noTaint)));
+
+		process.stdout.write(actual(left) + ' ' + op + ' ' +
+			actual(right) + ' = ' + result + '; ');
+		process.stdout.write(shadow(left, rule.noTaint) + ' ' + op + ' ' +
+			shadow(right, rule.noTaint) + ' = ' + taint_state + '\n');
+
+		return new AnnotatedValue(result, taint_state);
+	}
+
+
+	function binaryRec(left, right, taintProp, sandbox, iid)
+	{//todo
+		/*if (isTainted(shadow(left, noTaint)) || isTainted(shadow(right, noTaint)))
+		{
+			taintProp[(sandbox.iidToLocation(
+				sandbox.getGlobalIID(iid)))] = 1;
+		}*/
+	}
+	function funcInvokeRec(func, base, args)
+	{
+
+	}
+
+	function taintAll(val)
+	{
+		if (typeof val == 'object')
+		{//todo: should we change strategy
+			ret = Array.isArray(val) ? [] : {};
+			for (var k in val)
+			{
+				taintAll(val[k]);
+			}
+		}
+	}
+	function untaintAll()
+	{
+		//todo
+	}
+
+	function andTaintProp(left, right, result, op)
+	{
+		if (isZeroInBitOper(left) ||
+			isZeroInBitOper(right))
+		{
+			return result;
+		}
+		else
+		{
+			var taint_state = rule.arithmetic(
+				rule.compressTaint(shadow(left, rule.noTaint)),
+				rule.compressTaint(shadow(right, rule.noTaint)));
+
+			process.stdout.write(actual(left) + ' ' + op + ' ' +
+				actual(right) + ' = ' + result + '; ');
+			process.stdout.write(shadow(left, rule.noTaint) + ' ' + op + ' ' +
+				shadow(right, rule.noTaint) + ' = ' + taint_state + '\n');
+
+			return new AnnotatedValue(result, taint_state);
+		}
+	}
 	this.taintProp = {};
 	this.binaryPre = function(iid, op, left, right)
 	{
