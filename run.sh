f1304412317155cd765c103f6c7f0576c4d020ef
2019 <434791602@qq.com>
2019-05-23 18:10:31 +0100

    solve: because {xxx(a);const a = xxx;} cause exception

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-node jalangi2/src/js/commands/jalangi.js --inlineIID --inlineSource --analysis jalangi2/src/js/sample_analyses/ChainedAnalyses.js --analysis jalangi2/src/js/runtime/SMemory.js --analysis jalangi2/src/js/sample_analyses/dlint/Utils.js --analysis DynTaintAnalysis.js test.js
+node jalangi2/src/js/commands/jalangi.js --inlineIID --inlineSource --analysis jalangi2/src/js/sample_analyses/ChainedAnalyses.js --analysis jalangi2/src/js/runtime/SMemory.js --analysis jalangi2/src/js/sample_analyses/dlint/Utils.js --analysis DynTaintAnalysis.js tests/test.js
